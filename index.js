e04f5944cb30a0bfec020a6b982ba79c6431afb2
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-26 11:09:19 -0500

    Configurar en el index.js, el servidor graphql, variable entorno, cors, puerto de escucha y funcion para uqe el servidor pueda iniciar

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 import conectarBD from "./db/db.js";
 import express from "express";
 import cors from 'cors';
-import ApolloServer from 'apollo-server-express';
+import {ApolloServer} from 'apollo-server-express';
 import dotenv from 'dotenv';
+import {typeDefs} from './graphql/types.js';
+import {resolvers} from './graphql/resolvers.js'
 
+//para que nos deje utilizar las variables de entorno en toda la aplicacion
 dotenv.config();
 
+//Definir servidor GraphQL
+const server = new ApolloServer({
+    //Definiciones de cada uno de los modelos
+    typeDefs: typeDefs,
+    //Controladores
+    resolvers: resolvers,
+});
 
-const main = async () => {
+//Aplicacion express
+const app = express();
+
+//Middleware express.json, para que los request entren y salgan de tipo json
+app.use(express.json());
+
+//Middleware de cors; para poder hacer desde muchos origenes
+app.use(cors());
+
+//Puerto de escucha, onde corre el servidor
+app.listen({port: process.env.PORT || 4000}, async () => {
     await conectarBD();
-};
+    await server.start();
 
-main();
+    server.applyMiddleware({ app });
+
+    console.log('Servidor Listo!!');
+});
