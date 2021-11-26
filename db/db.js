d37c606262d9864e12fcaceb162ebadaaed3573c
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-26 09:44:11 -0500

    Se cambia string de conecion a la BD de ReactSoft

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const conectarBD = async ()=>{
     return await mongoose.connect(
-        'mongodb+srv://alramirez:UyrEMrha8bgqUTfS@gestiondeproyectos.yepmn.mongodb.net/GestionDeProyectos?retryWrites=true&w=majority'
+        'mongodb+srv://reactsoft:ReactSoftt@gestionproyectosmisiont.hjmim.mongodb.net/GestionProyectos?retryWrites=true&w=majority'
     ).then(()=>{
         console.log('Conexion a la BD Exitosa!!');
     }).catch(e=>{
