f63c97e8c1c2891ee2b6cb634d9fd9e46037fca4
Leonardo Ramirez <angelr521@hotmail.com>
2021-11-26 10:02:44 -0500

    Instalacion libreria de variables de entorno dotenv: yarn add dotenv

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import conectarBD from "./db/db.js";
+import { Express } from "express";
+import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+
+
 
 
 const main = async () => {
